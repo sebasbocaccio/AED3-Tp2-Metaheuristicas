858f7344feac477d4b80cb3ae1389848b2e593a4
mzuleta <mzuleta@dc.uba.ar>
2020-11-12 00:44:55 -0300

    Agrego Heuristica_1

--- Grafo/main.cpp
+++ Grafo/main.cpp
@@ -1,58 +1,119 @@
 #include <iostream>
 #include <list>
 #include <vector>
-class pair{
-public :
-    pair(int a,int b) : adyacente(a),peso(b) {}
-    int adyacente;
-    int peso;
-};
+#include <utility>
+#include <algorithm>
 
-class grafo{
+using namespace std;
+
+class grafo {
 
 public:
-    int numerito;
-    int cantDeNodos(){return _cantNodos ;}
-    int sizeVector(){return relaciones.size();}
-    grafo(int nodos) : _cantNodos(nodos){
-            int cant_relaciones;
-            int src;
-            int dst;
-            int peso;
-            relaciones = std::vector<std::vector<pair>> (_cantNodos);
-            std::cout << "Ingrese cantidad de relaciones" << std::endl;
-            std::cin >> cant_relaciones;
-            std::cout << "Vaya ingresandolas de forma v1 v2 e" << std::endl;
-            for(int i = 0 ; i < cant_relaciones ; i++){
-                std::cin >> src >> dst >> peso;
-                relaciones[src].push_back(pair(dst,peso));
-                relaciones[dst].push_back(pair(src,peso));
-            }
+    int cantDeNodos() { return _cantNodos; }
+
+    int sizeVector() { return relaciones.size(); }
+
+    int grado(const int &nodo) { return relaciones[nodo].size(); }
+
+    vector<vector<int>> listaAdyacencia() { return relaciones; }
+
+
+    grafo(int nodos, int aristas) : _cantNodos(nodos), _cantAristas(aristas) {
+        int src;
+        int dst;
+        relaciones = std::vector<std::vector<int>>(_cantNodos);
+        std::cout << "Vaya ingresandolas de forma v1 v2 e" << std::endl;
+        for (int i = 0; i < aristas; i++) {
+            std::cin >> src >> dst;
+            relaciones[src-1].push_back(dst-1);
+            relaciones[dst-1].push_back(src-1);
+        }
 
 
     }
-    void mostrarAdyacencia(){
-        for( std::vector<pair> v : relaciones){
+
+    void mostrarAdyacencia() {
+        for (std::vector<int> v : relaciones) {
             std::cout << "[";
-            for( pair p : v){
-                std::cout << p.adyacente << ",";
+            for (int i = 0; i < v.size() - 1; ++i) {
+                std::cout << v[i] << ", ";
             }
-            std::cout << "]" << std::endl ;
+            std::cout << v[v.size() - 1] << "]";
         }
 
     }
-private:
-    int _cantNodos;
-    std::vector<std::vector<pair>> relaciones;
-};
-int main() {
-    int cant_vertices;
-    std::cin >> cant_vertices;
-    grafo grafito = grafo(cant_vertices);
-    grafito.cantDeNodos();
-    grafito.mostrarAdyacencia();
-    std::list<pair> listita;
-    std::cout << grafito.sizeVector() << std::endl;
 
+private:
+     int &_cantAristas;
+     int &_cantNodos;
+    std::vector<std::vector<int>> relaciones;
+};
+
+
+
+
+bool colorValido(grafo G, vector<int> colores, int nodo, int color) {
+    vector<int> listaAdyacencia = G.listaAdyacencia()[nodo];
+    if(colores[nodo] != -1){
+       return false;
+    }
+    for (int i = 0; i < listaAdyacencia.size(); ++i) {
+        if (colores[listaAdyacencia[i]] == color)
+            return false;
+    }
+    return true;
+}
+
+
+void maximoImpacto(grafo G, grafo H) {
+    vector<int> colores(H.cantDeNodos(), -1);    // colores de cada nodo, -1 si no esta coloreado
+    vector<pair<int, int>> grados;
+    // ordeno los vertices por grado
+    for (int i = 0; i < H.cantDeNodos(); ++i) {
+        grados.push_back(make_pair(H.grado(i), i));
+    }
+    sort(grados.begin(), grados.end());
+
+    // posible optimizacion con priorida de colores
+
+    for (int i = H.cantDeNodos()-1; i >= 0; --i) {
+        if(colores[grados[i].second] == -1) {   // me fijo si i ya esta coloreado
+            colores[grados[i].second] = i;
+            vector<int> listaAdyacencia = H.listaAdyacencia()[grados[i].second];
+            // recorro los adyacentes al vertice i
+            for (int j = 0; j < listaAdyacencia.size(); ++j) {
+                if (colorValido(G, colores, listaAdyacencia[j], colores[grados[i].second])) {
+                    colores[listaAdyacencia[j]] = colores[grados[i].second];
+                }
+            }
+        }
+    }
+
+    // asigno colores distintos a los que quedan sin colorear
+    int colorDistinto = -2;
+    for (int i = 0; i < colores.size(); ++i) {
+        if(colores[i] == -1){
+            colores[i] = colorDistinto;
+            colorDistinto--;
+        }
+    }
+
+    for (int i = 0; i < colores.size(); ++i) {
+        cout << colores[i] << ", ";
+    }
+
+}
+
+
+int main() {
+    int cant_vertices = 0;
+    int cant_aristas_G = 0;
+    int cant_aristas_H = 0;
+    std::cin >> cant_vertices;
+    std::cin >> cant_aristas_G;
+    std::cin >> cant_aristas_H;
+    grafo G = grafo(cant_vertices, cant_aristas_G);
+    grafo H = grafo(cant_vertices, cant_aristas_H);
+    maximoImpacto(G, H);
     return 0;
 }
